38d0257d4064714a9f3e6172839c92d99d56d42e
Fredeeb <frederikbraendstrup@gmail.com>
2019-05-10 14:52:00 +0200

    Revision may 2019

--- elisp/utilties.el
+++ elisp/utilties.el
@@ -5,3 +5,12 @@
     (delete-frame)
     (split-window (selected-window) nil nil)
     (switch-to-buffer bn)))
+
+(defun insert-output-of-executed-line ()
+  "executes line at point in default shell and inserts stdout"
+  (interactive)
+  (insert
+   (shell-command-to-string
+    (delete-and-extract-region
+     (point-at-bol)
+     (point-at-eol)))))
