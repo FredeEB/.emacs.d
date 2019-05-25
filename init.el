6b166ba7e7b7d6419b8eb7535e31afcd76fa2467
Fredeeb <frederikbraendstrup@gmail.com>
2019-05-25 16:32:00 +0200

    :defer t

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
 ;(package-initialize)
 ;; Glorious oneliners!
+(setq gc-cons-threshold (* 50 1000 1000))
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(setq gc-cons-threshold (* 2 1000 1000))
