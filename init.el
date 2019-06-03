bdb79c37bd7be5d0e6986f8d5d5eccf834f7a63c
fredeeb <frederikbraendstrup@gmail.com>
2019-06-03 23:46:00 +0100

    journals

--- init.el
+++ init.el
@@ -1,5 +1,4 @@
-;(package-initialize)
-;; Glorious oneliners!
-(setq gc-cons-threshold (* 100 1000 1000))
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-(setq gc-cons-threshold (* 2 1000 1000))
+(when (not (file-exists-p (expand-file-name "~/.emacs.d/config.el")))
+  (require 'org)
+  (org-babel-tangle-file (expand-file-name "~/.emacs.d/config.org") (expand-file-name "~/.emacs.d/config.el")))
+(load-file (expand-file-name "~/.emacs.d/config.el"))
