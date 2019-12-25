6bd2abaa13ce68e9809fcede283ad47e8726bc88
Frede Braendstrup <im@fredeb.dev>
2019-12-25 12:16:38 +0100

    made init.el more agnostic

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
 ;; set garbage collection threshold high for initialization
 (setq gc-cons-threshold (* 100 1000 1000))
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 (setq gc-cons-threshold (* 2 1000 1000))
