f4343f4c382e0b5601bdb6f49284f8c5b96d0e77
fredeb <im@fredeb.dev>
2020-05-04 21:15:58 +0200

    disabled setting gc to default after emacs init

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
 ;; set garbage collection threshold high for initialization
 (setq gc-cons-threshold (* 100 1000 1000))
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-(setq gc-cons-threshold (* 2 1000 1000))
