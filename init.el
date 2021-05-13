7005582d7f93b763e719351a7d8ff7509ab797c5
Frede Braendstrup <frederikbraendstrup@gmail.com>
2021-05-13 15:02:37 +0200

    moved package management to init.el

--- init.el
+++ init.el
@@ -1,3 +1,26 @@
 ;; set garbage collection threshold high for initialization
-(setq gc-cons-threshold (* 100 1000 1000))
+(setq warning-suppress-types '((comp))
+	  gc-cons-threshold (* 100 1000 1000))
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+	   (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+	  (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+	(with-current-buffer
+		(url-retrieve-synchronously
+		 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+		 'silent 'inhibit-cookies)
+	  (goto-char (point-max))
+	  (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default t)
+
+(straight-use-package 'use-package)
+
+(use-package org)
+(use-package org-plus-contrib)
+
+;; load remaining config
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
